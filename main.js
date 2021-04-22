3d62d1ebf3a9efea1e196e6ffd2d05d86571041c
Mellow-Cruder <royalettus@gmail.com>
2021-04-22 12:47:34 -0400

    agora integrated...

--- main.js
+++ main.js
@@ -7,3 +7,6 @@ function closeNav() {
   document.getElementById("Sidenav").style.width = "0";
   document.getElementById("mask").style.width = "0";
 }
+
+import AgoraRTC from "agora-rtc-sdk-ng"
+const client = AgoraRTC.createClient({ mode: "live", codec: "vp8" });
